068e9f2d078fabe41f389decf0c026f208437117
nslebruh <nslebrun1@gmail.com>
2022-10-06 12:23:33 +1100

    error modal fix

--- src/app.jsx
+++ src/app.jsx
@@ -14,6 +14,7 @@ import { Communications } from "./pages/Communications"
 import { Subjects } from "./pages/Subjects"
 import { Subject } from "./pages/Subject"
 import { Profile } from "./pages/Profile"
+import { ErrorModal } from "./components/ErrorModal"
 import "./scss/index.scss"
 
 const Layout = () => {
